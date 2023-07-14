8c32349434007733c79e89e99c3fd0aa1135663f
21mebrat <21mebrat>
2023-07-14 02:14:30 -0700

    commit message on the c programming

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
-/* the function main diplays or prints the alphabates and returns zero*/
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: zero (Success)
+ */
 int main(void)
 {
-	char le[26] = "abcdefghijklmnopqrstuvwxyz";
+	char leter[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
 	for (i = 0; i < 26; i++)
 	{
-		putchar(le[i]);
+		putchar(leter[i]);
 	}
 	putchar('\n');
 	return (0);
