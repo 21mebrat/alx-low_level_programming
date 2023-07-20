d993bb7b49905c2c89539c6b67635ac3ba3bf11a
21mebrat <maytotmat@gmail.com>
2023-07-20 09:07:23 +0300

    commit for 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isalpha - Checks for alphabetic order
+ * Return: 1 if c is a letter, if not returns 0
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
+
