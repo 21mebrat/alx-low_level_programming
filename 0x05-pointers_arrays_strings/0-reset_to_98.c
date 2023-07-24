35129881c3c8099cb0719573404610b4cc0fdd0a
21mebrat <maytotmat@gmail.com>
2023-07-25 02:25:00 +0300

    commiting the 0-reset_to_98.c file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * reset_to_98 - a function It updates the value of the integer provided pointer * and sets it to 98
+ * reset_to_98 - a function It updates the value of the integer
+ * and sets it to 98
  * @n: Pointer to an integer whose value needs to be updated to 98.
  * Return: n
  */
