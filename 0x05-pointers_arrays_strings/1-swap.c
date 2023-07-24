8fc5ecfc41e3c479e5054617a2dd62a9ef0804cd
21mebrat <maytotmat@gmail.com>
2023-07-25 02:43:41 +0300

    commiting the 0-reset_to_98.c file

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-
  * swap_int - swaps the values of two integers
  * @a: Pointer to the first integer to be swapped.
  * @b: Pointer to the second integer to be swapped.
- */
+ *\
+
 void swap_int(int *a, int *b) {
     int temp = *a;
     *a = *b;
