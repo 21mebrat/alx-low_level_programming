2a4502c4ccd87aa735804a0a841e4ddd48de33b7
21mebrat <maytotmat@gmail.com>
2023-07-25 03:26:39 +0300

    commiting str.c file

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _strlen - Returns the length of a string.
- * @s: The input string.
- * Return: The length of the string.
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
  */
 int _strlen(char *s)
 {
