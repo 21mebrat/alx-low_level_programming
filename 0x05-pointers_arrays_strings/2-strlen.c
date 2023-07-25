2ffed3d6a14241f6a24e9f7747263ee3f88c23c4
21mebrat <maytotmat@gmail.com>
2023-07-25 03:22:09 +0300

    commiting str.c file

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strlen - Returns the length of a string.
  * @s: The input string.
@@ -8,6 +7,7 @@
 int _strlen(char *s)
 {
     int length = 0;
+
     while (*s != '\0')
     {
         length++;
